cd5773c9168442f6a920a9e2d909d9798c1e26a0
baarath <baarathsrinivas@gmail.com>
2020-04-30 00:10:33 +0530

    preview added

---
+++ derma/static/derma/js/main.js
@@ -0,0 +1,17 @@
+function loader() {
+	const input = document.getElementById('inputfile');
+	const image = document.getElementById('image');
+	const preview = document.getElementById('preview');
+	//console.log("called");
+	const file = input.files[0];
+	if(file){
+		const reader = new FileReader();
+		image.style.display = 'block';
+		preview.style.display = 'none';
+		reader.addEventListener("load",function(){
+			image.setAttribute("src",this.result);
+		});
+		reader.readAsDataURL(file);
+	}
+}
+
